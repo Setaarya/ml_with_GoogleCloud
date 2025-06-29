f07fb4ccd094cfae2dffffd35b272e68fc0add26
Aryaseta <146357135+Setaarya@users.noreply.github.com>
2025-06-29 13:37:58 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -3,17 +3,22 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: '0.0.0.0',
-    routes: {
-      payload: {
-        maxBytes: 1000000, // 1MB
-        output: 'stream',
-        parse: true,
-        allow: 'multipart/form-data'
-      }
+  port: 3000,
+  host: '0.0.0.0',
+  routes: {
+    cors: {
+      origin: ['*'],
+      headers: ['Accept', 'Content-Type'],
+      additionalHeaders: ['X-Requested-With']
+    },
+    payload: {
+      maxBytes: 1000000,
+      output: 'stream',
+      parse: true,
+      allow: 'multipart/form-data'
     }
-  });
+  }
+});
 
   server.route(routes);
 
