17f2fc20873ec29298007a29a6929b7add33fae7
Aryaseta <146357135+Setaarya@users.noreply.github.com>
2025-06-30 10:46:51 +0700

    Update inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -1,16 +1,19 @@
 const tf = require('@tensorflow/tfjs-node');
-const loadModel = require('./loadModel');
+const { getModel } = require('./loadModel');
 
-module.exports.predictImage = async (imageBuffer) => {
-  const model = await loadModel.getModel();
-  const imageTensor = tf.node.decodeImage(imageBuffer)
-    .resizeNearestNeighbor([224, 224])
-    .expandDims()
-    .toFloat()
-    .div(tf.scalar(255.0));
+async function predictImage(imageBuffer) {
+    const model = getModel();
 
-  const prediction = model.predict(imageTensor);
-  const score = (await prediction.data())[0];
+    const imageTensor = tf.node.decodeImage(imageBuffer)
+        .resizeNearestNeighbor([224, 224])
+        .expandDims()
+        .toFloat()
+        .div(tf.scalar(255.0));
 
-  return score > 0.5 ? 'Cancer' : 'Non-cancer';
+    const prediction = model.predict(imageTensor);
+    const score = (await prediction.data())[0];
+
+    return score > 0.5 ? 'Cancer' : 'Non-cancer';
 };
+
+module.exports = { predictImage };
