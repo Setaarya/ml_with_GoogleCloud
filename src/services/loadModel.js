636ba6af735cdeae9dfe559c899d8bd768ca0c15
Aryaseta <146357135+Setaarya@users.noreply.github.com>
2025-06-30 10:49:05 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,20 @@
 const tf = require('@tensorflow/tfjs-node');
-async function loadModel() {
-    return tf.loadGraphModel('https://storage.googleapis.com/model_storage_yulianto/model-in-prod/model.json');
+
+let model;
+
+// Fungsi ini akan dipanggil sekali saat server start
+async function loadAppModel() {
+    console.log('Loading model from GCS...');
+    model = await tf.loadGraphModel('https://storage.googleapis.com/model_storage_yulianto/model-in-prod/model.json');
+    console.log('Model loaded successfully.');
 }
-module.exports = loadModel;
+
+// Fungsi ini akan dipanggil setiap kali ada request prediksi
+function getModel() {
+    if (!model) {
+        throw new Error('Model is not loaded yet!');
+    }
+    return model;
+}
+
+module.exports = { loadAppModel, getModel };
