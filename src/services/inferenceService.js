e5843572af87066b4a55649ed29ec563d197fd06
Aryaseta <146357135+Setaarya@users.noreply.github.com>
2025-06-30 14:26:27 +0700

    Update inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -4,13 +4,13 @@ const { getModel } = require('./loadModel');
 async function predictImage(imageBuffer) {
     const model = getModel();
 
-    const imageTensor = tf.node.decodeImage(imageBuffer, 3)
-        .resizeNearestNeighbor([224, 224])
-        .expandDims()
-        .toFloat()
-        .div(tf.scalar(255.0));
+    const tensor = tf.node
+            .decodeJpeg(image)
+            .resizeNearestNeighbor([224, 224])
+            .expandDims()
+            .toFloat()
 
-    const prediction = model.predict(imageTensor);
+    const prediction = model.predict(tensor);
     const score = await prediction.data();
     const confidenceScore = Math.max(...score) * 100;
  
