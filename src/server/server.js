48c45ba2408fe4fb4e74777cac9e64354a241a35
Aryaseta <146357135+Setaarya@users.noreply.github.com>
2025-06-30 10:48:06 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,28 +1,30 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const { loadAppModel } = require('../services/loadModel');
 
 const init = async () => {
+  await loadAppModel();
+
   const server = Hapi.server({
-  port: 3000,
-  host: '0.0.0.0',
-  routes: {
-    cors: {
-      origin: ['*'],
-      headers: ['Accept', 'Content-Type'],
-      additionalHeaders: ['X-Requested-With']
-    },
-    payload: {
-      maxBytes: 1000000,
-      output: 'stream',
-      parse: true,
-      multipart: true,
-      allow: 'multipart/form-data'
+    port: 3000,
+    host: '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+        headers: ['Accept', 'Content-Type'],
+        additionalHeaders: ['X-Requested-With']
+      },
+      payload: {
+        maxBytes: 1000000,
+        output: 'stream',
+        parse: true,
+        multipart: true,
+        allow: 'multipart/form-data'
+      }
     }
-  }
-});
+  });
 
   server.route(routes);
-
   await server.start();
   console.log('Server running on %s', server.info.uri);
 };
